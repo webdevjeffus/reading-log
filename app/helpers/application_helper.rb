5f26585eea881aeff6adbca726783f93bb3cd975
Jeff George <webdevjeffus@gmail.com>
2016-03-11 16:52:48 -0500

    Add date helpers to application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def std_format_date date
+    return "" unless date
+    date.strftime(" at %l:%M %P on %b %e, %Y")
+  end
+
+  def std_format_day date
+    return "" unless date
+    date.strftime("%b %e, %Y")
+  end
+
 end
