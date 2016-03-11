ae1b9eb212866ec18fc52552c782bd29b683b7cc
Jeff George <webdevjeffus@gmail.com>
2016-03-11 16:15:18 -0500

    Add resource routes for users & sessions to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :users, only: [ :new, :show, :create, :index ]
+  resources :sessions, only: [ :new, :create, :destroy ]
+
 end
