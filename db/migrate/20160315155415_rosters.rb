b4f9c11aaf5b382fb92a600052385fc481ac948e
Jeff George <webdevjeffus@gmail.com>
2016-03-15 14:28:29 -0400

    Fix bug in rosters migration

--- db/migrate/20160315155415_rosters.rb
+++ db/migrate/20160315155415_rosters.rb
@@ -1,10 +1,12 @@
 class Rosters < ActiveRecord::Migration
   def change
-    t.string      :name, limit: 50, null: false
-    t.text        :description # optional field
+    create_table :users do |t|
+      t.string      :name, limit: 50, null: false
+      t.text        :description # optional field
 
-    t.integer     :user_id, null: false, index: true
+      t.integer     :user_id, null: false, index: true
 
-    t.timestamps  null: false
+      t.timestamps  null: false
+    end
   end
 end
