1a265351da24465f4a11e413819b2246d5f7c52f
Jeff George <webdevjeffus@gmail.com>
2016-03-15 14:37:42 -0400

    Update schema.rb to reflect readers and rosters migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311205514) do
+ActiveRecord::Schema.define(version: 20160315174226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "readers", force: :cascade do |t|
+    t.string   "username",      limit: 20, null: false
+    t.string   "password",      limit: 20
+    t.integer  "user_id",                  null: false
+    t.string   "reading_level", limit: 12
+    t.string   "firstname",     limit: 24, null: false
+    t.string   "lastname",      limit: 24, null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "readers", ["user_id"], name: "index_readers_on_user_id", using: :btree
+
+  create_table "rosters", force: :cascade do |t|
+    t.string   "name",        limit: 50, null: false
+    t.text     "description"
+    t.integer  "user_id",                null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "rosters", ["user_id"], name: "index_rosters_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "firstname",       limit: 24, null: false
