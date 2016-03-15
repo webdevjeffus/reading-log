4f89bb5c8955ae9dd324071993e3be2f4ff732be
Jeff George <webdevjeffus@gmail.com>
2016-03-15 13:44:52 -0400

    Add validates uniqueness for user.username in users migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 
   validates :firstname, presence: true, length: {in: 2..24}
   validates :lastname, presence: true, length: {in: 1..24}
-  validates :username, presence: true, length: {in: 8..20}
+  validates :username, presence: true, uniqueness: true, length: {in: 8..20}
   validates :email, presence: true
 
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
