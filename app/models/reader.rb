d928f0593819bc3804ba73abb8538fdaf39c2720
Jeff George <webdevjeffus@gmail.com>
2016-03-16 16:06:27 -0400

    Add Reader#name and #name? methods to Reader model

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -13,4 +13,27 @@ class Reader < ActiveRecord::Base
   validates   :firstname, allow_nil: true, length: {in: 2..24}
   validates   :lastname, allow_nil: true, length: {in: 1..24}
 
+
+  def name?
+    self.firstname || self.lastname
+  end
+
+  def name
+    if self.firstname || self.lastname
+      name = ""
+      if self.firstname
+        name += self.firstname
+      end
+      if self.firstname && self.lastname
+        name += " "
+      end
+      if self.lastname
+        name += self.lastname
+      end
+    else
+      name = self.username
+    end
+    name
+  end
+
 end
