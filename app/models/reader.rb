2e2358c86e7fd06ce23724bc7a7bf06c260acb14
Jeff George <webdevjeffus@gmail.com>
2016-03-16 15:43:13 -0400

    Add has_many through: between readers and rosters through logs

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,15 +1,16 @@
 class Reader < ActiveRecord::Base
   belongs_to  :user
-  has_many  :logs
+  has_many    :logs
+  has_many    :rosters, through: :logs
 
   # Required fields
-  validates :username, presence: true, length: {in: 3..20}
-  validates :password, presence: true, length: {in: 3..20}
-  validates :user, presence: true
+  validates   :username, presence: true, length: {in: 3..20}
+  validates   :password, presence: true, length: {in: 3..20}
+  validates   :user, presence: true
 
   # Optional fields
-  validates :reading_level, allow_nil: true, length: {in: 1..12}
-  validates :firstname, allow_nil: true, length: {in: 2..24}
-  validates :lastname, allow_nil: true, length: {in: 1..24}
+  validates   :reading_level, allow_nil: true, length: {in: 1..12}
+  validates   :firstname, allow_nil: true, length: {in: 2..24}
+  validates   :lastname, allow_nil: true, length: {in: 1..24}
 
 end
