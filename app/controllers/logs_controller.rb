357908eef62e2766b15d3567241ff232a2be946e
Jeff George <webdevjeffus@gmail.com>
2016-03-17 17:23:12 -0400

    Implement logs#create route, add code in logs#new to hide readers already on roster

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -6,11 +6,35 @@ class LogsController < ApplicationController
   end
 
   def new
-    binding.pry
     @roster = Roster.find_by(id: params[:roster_id])
+
+    # TODO: Refactor this out to a model (probably roster, since we are finding readers available for a given roster)
+    readers = Reader.where(user_id: @roster.user_id).order(:username)
+    @readers = []
+    readers.each do |reader|
+      @readers.push reader unless Log.find_by(roster: @roster, reader: reader)
+    end
+
     @log = @roster.logs.new
   end
 
+  def create
+    @log = Log.new(log_params)
+    @log.reader = Reader.find_by(id: params[:reader_id])
 
+    if @log.save
+      redirect_to roster_path(id: @log.roster_id)
+    else
+      @errors = @log.errors.full_messages
+      render new_logs_path
+    end
+  end
+
+
+  private
+
+  def log_params
+    log_params = params.require(:log).permit(:reader, :comment, :roster_id )
+  end
 
 end
